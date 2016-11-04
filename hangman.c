0ba427024eb98dd06ecb36da25189a5f9f6db261
Jesse Oberbeck <jesse.oberbeck@gmail.com>
2016-11-03 21:54:46 -0400

    base functionality mostly working. need to fix how wrongs are calculated, and add file functionality.

--- hangman.c
+++ hangman.c
@@ -1,24 +1,23 @@
 #include <stdio.h>
 #include <string.h>
-
-
-
-
+#include <stdlib.h>
 
 void clear_buffer(void)
 {
     while(getchar() != '\n');
 }
 
-int print_puzzle(char *word_buf, char *all_guesses, int word_len)
+void print_puzzle(char *word_buf, char *all_guesses, int word_len, int *prev_correct, int *wrong)
 {
     int printed = 0;
+    int correct = 0;
     printf("allguess: %s\n", all_guesses);
     for(int i = 0; i < word_len; i++){
         for(int i2 = 0; all_guesses[i2] != '\0'; i2++){
             if(word_buf[i] == all_guesses[i2]){
                 printf("%c", word_buf[i]);
                 printed = 1;
+                correct++;
                 break;
             }
         }
@@ -27,6 +26,19 @@ int print_puzzle(char *word_buf, char *all_guesses, int word_len)
             printed = 0;
         }
         printed = 0;
+    }
+    /*
+     if(*prev_correct == correct){
+        *wrong += 1;
+    }
+    */
+    //printf("correct: %d\n", correct);
+    int wrongs = (strlen(all_guesses) - correct);
+    if(wrongs == 6){
+        puts("you lose...");
+        exit();
+    }else{
+        printf("Wrong: %d\n", wrongs);
     }
     puts("");
     return;
@@ -40,7 +52,8 @@ void collect_input(char *user_guess, char *all_guesses)
     clear_buffer();
     int i = 0;
     while(all_guesses[i] != '\0'){
-        if(strcmp(user_guess, &all_guesses[i]) == 0){
+        puts("comparing");
+        if(*user_guess == all_guesses[i]){
             return;
         }
         i++;
@@ -58,14 +71,13 @@ int main(int argc, char *argv[])
     char user_guess;
     char all_guesses[32] = {'\0'};
     int wrong = 0;
-    int output = 0;
+    int correct = 0;
     printf("%d\n", word_len);
     while(1){
-        output = print_puzzle(&word_buf, &all_guesses, word_len);
-        if(output){
-            wrong++;
-        }
+        print_puzzle(&word_buf, &all_guesses, word_len, &correct, &wrong);
+
         collect_input(&user_guess, all_guesses);
         printf("guess in main: %c\n",user_guess);
+        //printf("wrong: %d\n", wrong);
     }
 }
