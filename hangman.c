61817f7d363407a1fceabf4cf10b72a7de20d4b0
Jesse Oberbeck <jesse.oberbeck@gmail.com>
2016-11-04 12:09:46 -0400

    Added function to read in file from relative path. It's not working yet.

--- hangman.c
+++ hangman.c
@@ -1,19 +1,36 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 void clear_buffer(void)
 {
     while(getchar() != '\n');
 }
 
+void read_file(char *word_buf)
+{
+    char *path = getenv("HOME");
+    char filename[32];
+    snprintf(filename, sizeof(filename), "%s/.words", path);
+    char line[32];
+    FILE *word_list = fopen(path, "r");
+    
+    if(!word_list){
+        perror("File failed to open.");
+        return;
+    }
+    fgets(line, sizeof(line), word_list);
+    printf("wordbuf post file: %s\n", filename);
+    fclose(word_list);
+}
 
 void print_puzzle(char *word_buf, char *all_guesses, int word_len)
 {
     int dash_count = 0;
     int printed = 0;
     int correct = 0;
-    printf("allguess: %s\n", all_guesses);
+    //printf("allguess: %s\n", all_guesses);
     for(int i = 0; i < word_len; i++){
         for(int i2 = 0; all_guesses[i2] != '\0'; i2++){
             if(word_buf[i] == all_guesses[i2]){
@@ -80,16 +97,27 @@ int main(int argc, char *argv[])
 {
     int wrongs = 0;
     char word_buf[128] = {"testing"};
-    int word_len = strlen(word_buf);
     char user_guess;
     char all_guesses[32] = {'\0'};
-
-    printf("%d\n", word_len);
+    
+    if(argc == 2){
+        strncpy(word_buf, argv[1], sizeof(word_buf));
+    }else{
+        if(access("~/.words", F_OK)){
+            puts("File found.");
+            read_file(word_buf);
+        }
+        
+    }
+    
+    int word_len = strlen(word_buf);
+    
+    printf("There are %d letters.\n", word_len);
     while(wrongs < 6){
         print_puzzle(word_buf, all_guesses, word_len);
         collect_input(&user_guess, all_guesses);
         count_wrong(word_buf, all_guesses, word_len, &wrongs);
-        printf("wrong: %d\n", wrongs);
+        //printf("wrong: %d\n", wrongs);
     }
     puts("You lose...");
 }
